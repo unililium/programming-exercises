8a0ad8725bca022f365c3b3257929a22bed1be8d
Du Lin <lnidux@gmail.com>
2017-01-12 18:22:44 +0100

    First commit

---
+++ _.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<fstream>
+#include<strings>
+#include<vector>
+
+using namespace std;
+
+int main()
+{
+    cout
+}
---
+++ analisi_frequenze.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+class Symbol {
+    char car;
+    int q;
+public:
+    Symbol(char car, int q) : car(car), q(q) {}
+};
+
+int main() {
+    ofstream file;
+    string pathname;
+
+    cout << "Insert the full pathname of the file: " << endl;
+    cin >> pathname;
+    file.open(pathname);
+    if(file.is_open()) {
+
+    } else {
+        cout << "error" << endl;
+    }
+    file.close();
+
+    return 1;
+}
