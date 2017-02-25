1c16791ed24ee1c7d0a03d1828b6dcef21fffd6e
Du Lin <lnidux@gmail.com>
2017-02-25 21:45:06 +0100

    lists1.rkt Racket

---
+++ Racket/lists1.rkt
@@ -0,0 +1,20 @@
+#lang racket
+
+(define (my-last l)
+   (if (null? (cdr l))
+       (car l)
+       (my-last (cdr l))))
+
+(define (my-butlast l)
+  (if (null? (cdr l))
+      '()
+      (if (null? (cddr l))
+          (car l)
+          (my-butlast (cdr l)))))
+
+(define (element-at l n)
+  (if (> n (length l))
+      (printf "n gt length l")
+      (if (= n 1)
+          (car l)
+          (element-at (cdr l) (- n 1)))))
