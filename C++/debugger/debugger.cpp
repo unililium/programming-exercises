0a15de14762ebcfb9b60a4fc32c1db29a4335323
Du Lin <lnidux@gmail.com>
2017-03-25 20:25:45 +0100

    debugger

--- C++/debugger/debugger.cpp
+++ C++/debugger/debugger.cpp
@@ -1,5 +1,20 @@
-#include <iostream>
+#include <sys/ptrace.h>
 #include <unistd.h>
+#include <cstring>
+#include <iostream>
+#include <string>
+
+class debugger {
+   public:
+    debugger(std::string prog_name, pid_t pid)
+        : m_prog_name{std::move(prog_name)}, m_pid{pid} {}
+
+    void run();
+
+   private:
+    std::string m_prog_name;
+    pid_t m_pid;
+};
 
 int main(int argc, char* argv[]) {
     if (argc < 2) {
@@ -7,10 +22,14 @@ int main(int argc, char* argv[]) {
         return -1;
     }
 
-    auto prog = argv[1];
+    std::string prog = argv[1];
 
     auto pid = fork();
     if (pid == 0) {
+        ptrace(PTRACE_TRACEME, 0, nullptr, nullptr);
+        execl(prog.c_str(), prog.c_str(), nullptr);
     } else if (pid >= 1) {
+        debugger dbg(prog, pid);
+        dbg.run();
     }
 }
