4dd6e9d59fad94e2a77e9d7b06e5b97b19282419
Du Lin <lnidux@gmail.com>
2017-03-27 13:46:58 +0200

    dbg linoise

--- C++/debugger/debugger.cpp
+++ C++/debugger/debugger.cpp
@@ -1,4 +1,6 @@
+#include <linenoise.h>
 #include <sys/ptrace.h>
+#include <sys/wait.h>
 #include <unistd.h>
 #include <cstring>
 #include <iostream>
@@ -14,7 +16,23 @@ class debugger {
    private:
     std::string m_prog_name;
     pid_t m_pid;
+    void handle_command(const std::string&);
 };
+
+void debugger::run() {
+    int wait_status;
+    auto options = 0;
+    waitpid(m_pid, &wait_status, options);
+
+    char* line = nullptr;
+    while ((line = linenoise("minidbg> ")) != nullptr) {
+        handle_command(line);
+        linenoiseHistoryAdd(line);
+        linenoiseFree(line);
+    }
+}
+
+void debugger::handle_command(const std::string& line) {}
 
 int main(int argc, char* argv[]) {
     if (argc < 2) {
