58744abf87c161d083f9b4b587f12fa58767b95b
Du Lin <lnidux@gmail.com>
2017-04-12 15:08:07 +0200

    WebGL first example

--- WebGL/drawing.js
+++ WebGL/drawing.js
@@ -1,2 +1,16 @@
 function main() {
     var canvas = document.getElementById("my-canvas");
+    var context = canvas.getContext("2d");
+
+   for(var x = 0.5; x <= 800; x += 10) {
+       context.moveTo(x, 0);
+       context.lineTo(x, 600);
+   }
+
+   for(var y = 0.5; y <= 600; y += 10) {
+       context.moveTo(0, y);
+       context.lineTo(800, y);
+   }
+    context.strokeStyle = "#e43298";
+    context.stroke();
+}
