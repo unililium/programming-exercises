2622ef811a31d345024c38ff8c60fa293b0f7819
Du Lin <lnidux@gmail.com>
2017-04-19 13:49:47 +0200

    .

--- parallel-programming/OpenMP/openmphello.cpp
+++ parallel-programming/OpenMP/openmphello.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main() {
     int tid;
 
-    #pragma omp parallel num_threads(3) private(tid)
+#pragma omp parallel num_threads(3) private(tid)
     {
         tid = omp_get_thread_num();
         cout << "Hello World. I am thread " << tid << endl;
