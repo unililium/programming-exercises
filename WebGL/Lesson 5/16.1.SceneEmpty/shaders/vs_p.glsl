6e05650962428b8932de591b9d683f78a08fbd38
Du Lin <lnidux@gmail.com>
2017-06-12 12:54:09 +0200

    webgl lesson 5 UVs

--- WebGL/Lesson 5/16.1.SceneEmpty/shaders/vs_p.glsl
+++ WebGL/Lesson 5/16.1.SceneEmpty/shaders/vs_p.glsl
@@ -1,15 +1,17 @@
-attribute vec3 inPosition; 
-attribute vec3 inNormal; 
+attribute vec3 inPosition;
+attribute vec3 inNormal;
+attribute vec2 inUVs;
 
-varying vec3 fsNormal; 
-varying vec3 fsPosition; 
+varying vec3 fsNormal;
+varying vec3 fsPosition;
+varying vec2 fsUVs;
 
-uniform mat4 wvpMatrix; 
+uniform mat4 wvpMatrix;
 
 
-void main() { 
-	fsNormal = inNormal; 
+void main() {
+	fsNormal = inNormal;
 	fsPosition =  inPosition;
+	fsUVs = inUVs;
 	gl_Position = wvpMatrix * vec4(inPosition, 1.0);
 }
-	
