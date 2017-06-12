3d4d97ae6d351e46bdd97b85f87a305d9974dd9a
Du Lin <lnidux@gmail.com>
2017-06-12 12:56:32 +0200

    webgl lesson 5 fs texture

--- WebGL/Lesson 5/16.1.SceneEmpty/shaders/fs_p.glsl
+++ WebGL/Lesson 5/16.1.SceneEmpty/shaders/fs_p.glsl
@@ -1,28 +1,33 @@
-precision highp float; 
+precision highp float;
 
 uniform vec4 mDiffColor;
-uniform vec4 mSpecColor;            
+uniform vec4 mSpecColor;
 uniform float mSpecPower;
+
+uniform sampler2D textureFile;
+uniform float textureInfluence;
 
 uniform vec3 lightDirection;
 uniform vec4 lightColor;
 
 uniform vec3 eyePosition;
 
-varying vec3 fsNormal; 
-varying vec3 fsPosition; 
+varying vec3 fsNormal;
+varying vec3 fsPosition;
 
-void main() { 
+void main() {
 
 	vec3 nEyeDirection = normalize(eyePosition - fsPosition);
 	vec3 nlightDirection = - normalize(lightDirection);
 	vec3 nNormal = normalize(fsNormal);
 
-	vec4 diffuse =  mDiffColor * lightColor * clamp(dot(nlightDirection, nNormal), 0.0, 1.0);	
-	
+	vec4 diffuseTextureColorMixture = texture2D(textureFile, fsUVs) * textureInfluence;
+
+	vec4 diffuse =  mDiffColor * lightColor * clamp(dot(nlightDirection, nNormal), 0.0, 1.0);
+
 	vec3 hVec = normalize(nEyeDirection + nlightDirection);
 	vec4 specular = mSpecColor * lightColor * pow(clamp(dot(nNormal, hVec),0.0, 1.0), mSpecPower);
-	
-	gl_FragColor = min(diffuse + specular, vec4(1.0, 1.0, 1.0, 1.0)); 
+
+	gl_FragColor = min(diffuse + specular, vec4(1.0, 1.0, 1.0, 1.0));
 
 }
