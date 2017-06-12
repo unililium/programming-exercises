1a5a8d2a6bf0f4692b77cbde72a5b98ad0f3349c
Du Lin <lnidux@gmail.com>
2017-06-12 13:29:03 +0200

    webgl lesson 5 gouraud

--- WebGL/Lesson 5/16.1.SceneEmpty/shaders/fs_p.glsl
+++ WebGL/Lesson 5/16.1.SceneEmpty/shaders/fs_p.glsl
@@ -24,15 +24,15 @@ vec4 lightModel(int lt, vec3 pos) {
 
 	lDim = 1.0;
 
-	if (lt === 1) { //directional
+	if (lt == 1) { //directional
 		nLightDir = -normalize(lightDirection);
-	} else if (lt === 2) { //point light
+	} else if (lt == 2) { //point light
 		nLightDir = normalize(lightPosition - pos);
-	} else if (lt === 3) { // point light w. decay
+	} else if (lt == 3) { // point light w. decay
 		float lLen = length(lightPosition - pos);
 		nLightDir = normalize(lightPosition - pos);
 		lDim = 160.0 / (lLen * lLen);
-	} else if (lt === 4) { // spotlight
+	} else if (lt == 4) { // spotlight
 		nLightDir = normalize(lightPosition - pos);
 		lCone = -dot(nLightDir, normalize(lightDirection));
 		if (lCone < 0.5) {
@@ -43,6 +43,8 @@ vec4 lightModel(int lt, vec3 pos) {
 			lDim = (lCone - 0.5) / 0.2;
 		}
 	}
+
+	return ;
 }
 
 void main() {
@@ -59,8 +61,9 @@ void main() {
 
 	vec4 diffuse =  mDiffColor * lightColor * clamp(dot(nlightDirection, nNormal), 0.0, 1.0);
 
-	vec3 hVec = normalize(nEyeDirection + nlightDirection);
-	vec4 specular = mSpecColor * lightColor * pow(clamp(dot(nNormal, hVec),0.0, 1.0), mSpecPower);
+	//vec3 hVec = normalize(nEyeDirection + nlightDirection);
+	vec3 reflection = -reflect(nlightDirection, nNormal);
+	vec4 specular = mSpecColor * lightColor * pow(clamp(dot(reflection, nEyeDirection),0.0, 1.0), mSpecPower);
 
 	gl_FragColor = min(diffuse + specular, vec4(1.0, 1.0, 1.0, 1.0));
 
--- WebGL/Lesson 5/16.1.SceneEmpty/shaders/vs_g.glsl
+++ WebGL/Lesson 5/16.1.SceneEmpty/shaders/vs_g.glsl
@@ -1,14 +1,33 @@
-attribute vec3 inPosition; 
-attribute vec3 inNormal; 
+attribute vec3 inPosition;
+attribute vec3 inNormal;
 
-varying vec3 fsNormal; 
+//varying vec3 fsNormal;
 
-uniform mat4 wvpMatrix; 
+uniform mat4 wvpMatrix;
 
+uniform vec4 mSpecColor;
+uniform float mSpecPower;
 
-void main() { 
-	fsNormal = inNormal; 
-	
+uniform vec3 lightDirection;
+uniform vec3 lightPosition;
+uniform vec4 lightColor;
+uniform int lightType;
+
+uniform vec3 eyePosition;
+varying vec2 fsUVs;
+varying vec4 gouraudSpecular;
+varying vec4 gouraudDiffuse;
+
+void main() {
+	fsUVs = inUVs;
+
+	fsNormal = inNormal;
+
 	gl_Position = wvpMatrix * vec4(inPosition, 1.0);
+
+	vec3 nEyeDirection = normalize(eyePosition - inPosition);
+	//vec3 nlightDirection = - normalize(lightDirection);
+	vec3 nNormal = normalize(inNormal);
+
+
 }
-	
