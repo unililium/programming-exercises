41ac1b7629c395363e1a13a1943ab5073ff10791
Du Lin <lnidux@gmail.com>
2017-06-12 13:11:43 +0200

    webgl lesson 5 ligjts

--- WebGL/Lesson 5/16.1.SceneEmpty/shaders/fs_p.glsl
+++ WebGL/Lesson 5/16.1.SceneEmpty/shaders/fs_p.glsl
@@ -8,20 +8,44 @@ uniform sampler2D textureFile;
 uniform float textureInfluence;
 
 uniform vec3 lightDirection;
+uniform vec3 lightPosition;
 uniform vec4 lightColor;
+uniform int lightType;
 
 uniform vec3 eyePosition;
 
+varying vec2 fsUVs;
 varying vec3 fsNormal;
 varying vec3 fsPosition;
+
+vec4 lightModel(int lt, vec3 pos) {
+	vec3 nLightDir;
+	float lDim, lCone;
+
+	lDim = 1.0;
+
+	if (lt === 1) { //directional
+		nLightDir = -normalize(lightDirection);
+	} else if (lt === 2) { //point light
+		nLightDir = normalize(lightPosition - pos);
+	} else if (lt === 3) { // point light w. decay
+		float lLen = length(lightPosition - pos);
+		nLightDir = normalize(lightPosition - pos);
+		lDim = a60.0 / (lLen * lLen);
+	}
+}
 
 void main() {
 
 	vec3 nEyeDirection = normalize(eyePosition - fsPosition);
-	vec3 nlightDirection = - normalize(lightDirection);
+	//vec3 nlightDirection = - normalize(lightDirection);
 	vec3 nNormal = normalize(fsNormal);
 
-	vec4 diffuseTextureColorMixture = texture2D(textureFile, fsUVs) * textureInfluence;
+	vec4 lm = lightModel(lightType, fsPosition);
+	vec3 nlightDirection = lm.rgb;
+	float lightDim = lm.a;
+
+	vec4 diffuseTextureColorMixture = texture2D(textureFile, fsUVs) * textureInfluence + mDiffColor * (1.0 - textureInfluence);
 
 	vec4 diffuse =  mDiffColor * lightColor * clamp(dot(nlightDirection, nNormal), 0.0, 1.0);
 
