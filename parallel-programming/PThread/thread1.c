064966f1616f6b2eb62ca8adb1133ec2b8bd5924
Du Lin <lnidux@gmail.com>
2017-07-11 14:37:27 +0200

    thread1.c

---
+++ parallel-programming/PThread/thread1.c
@@ -0,0 +1,34 @@
+#include <pthread.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+
+void *thread_function(void *arg) {
+    
+    int i;
+ 
+    for (i = 0; i < 20; i++) {
+        printf("Thread says hi!\n");
+        sleep(1);
+    }
+
+    return NULL;
+}
+
+int main(void) {
+    
+    pthread_t mythread;
+    
+    if (pthrad_create(&mythread, NULL, thread_function, NULL)) {
+        printf("error creating thread.");
+        abort();
+    }
+
+    if(pthread_jion (mythread, NULL)) {
+        printf("error joining thread.");
+        abort();
+    }
+
+    exit(0);
+
+}
