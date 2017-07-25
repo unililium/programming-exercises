a8e201abd70f9f7bf10f162aa3ea8954da6b0e17
Du Lin <lnidux@gmail.com>
2017-07-25 14:49:49 +0200

    linear_class1.m

--- machine-learning/MATLAB/linear_class1.m
+++ machine-learning/MATLAB/linear_class1.m
@@ -1,3 +1,5 @@
+clc
+clear
 load iris_dataset.mat;
 x = zscore(irisInputs([1 2], :)');
 t = irisTargets(1,:)';
@@ -5,10 +7,3 @@ gplotmatrix(x, [], t);
 
 net = perceptron;
 net = train(net, x', t');
-
-figure();
-gscatter(x(:,1),x(:,2),perc_t);
-hold on;
-s = -2:0.01:2.5;
-plot(s, -(s * net.IW{1}(1) + net.b{1} ) / net.IW{1}(2), 'k');
-plot(s, -(s * w(2) + w(1)) / w(3), 'r');
