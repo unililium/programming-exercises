9a23c751bcb6a1015361f4a6d517cedce3422dc7
Du Lin <lnidux@gmail.com>
2017-08-24 17:23:54 +0200

    erlang dolphin3

--- Erlang/dolphins.erl
+++ Erlang/dolphins.erl
@@ -17,3 +17,15 @@ dolphin2() ->
         _ ->
             io:format("Heh, we're smarter than you humans.~n")
     end.
+
+dolphin3() ->
+    receive
+        {From, do_a_flip} ->
+            From ! "How about no?",
+            dolphin3();
+        {From, fish} ->
+            From ! "So long and thanks for all the fish!";
+        _ ->
+            io:format("Heh, we're smarter than you humans.~n"),
+            dolphin3()
+    end.
