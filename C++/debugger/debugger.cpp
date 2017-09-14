c82727b2b6be63008011041f6800c0b8ca29591c
Du Lin <lnidux@gmail.com>
2017-09-14 11:39:24 +0200

    debugger

--- C++/debugger/debugger.cpp
+++ C++/debugger/debugger.cpp
@@ -17,6 +17,7 @@ class debugger {
     std::string m_prog_name;
     pid_t m_pid;
     void handle_command(const std::string&);
+    void continue_execution();
 };
 
 void debugger::run() {
@@ -32,7 +33,41 @@ void debugger::run() {
     }
 }
 
-void debugger::handle_command(const std::string& line) {}
+void debugger::continue_execution() {
+    ptrace(PTRACE_CONT, m_pid, nullptr, nullptr);
+
+    int wait_status;
+    auto options = 0;
+    waitpid(m_pid, &wait_status, options);
+}
+
+std::vector<std::string> split(const std::string& s, char delimiter) {
+    std::vector<std::string> out();
+    std::stringstrem ss(s);
+    std::string item;
+
+    while (std::getline(ss, item, delimiter)) {
+        out.push_back(item);
+    }
+
+    return out;
+}
+
+bool is_prefix(const std::string& s, const std::string& of) {
+    if (s.size() > of.size()) return false;
+    return std::equal(s.begin(), s.end(), of.begin());
+}
+
+void debugger::handle_command(const std::string& line) {
+    auto args = split(line, ' ');
+    auto command = args[0];
+
+    if (is_prefix(command, "continue")) {
+        continue_execution();
+    } else {
+        std::cerr << "Unknown command\n";
+    }
+}
 
 int main(int argc, char* argv[]) {
     if (argc < 2) {
