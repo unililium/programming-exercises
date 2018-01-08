dda0413ee911efbd97dcc49e479dbfe23e21a381
Du Lin <lnidux@gmail.com>
2018-01-08 17:37:53 +0100

    4 problems - Racket 99 problems

--- Racket/99.rkt
+++ Racket/99.rkt
@@ -1,3 +1,27 @@
 #lang racket
 
-(def (my-last l) ())
+(define (my-last l)
+  (if (null? (cdr l))
+      l
+      (my-last (cdr l))))
+(my-last '(a b c d))
+
+(define (my-but-last l)
+  (if (= 2 (length l))
+      l
+      (my-but-last (cdr l))))
+(my-but-last '(a b c d))
+
+(define (element-at l n)
+  (if (= n 1)
+      (car l)
+      (element-at (cdr l) (sub1 n))))
+(element-at '(a b c d) 3)
+
+(define (my-length l)
+  (define (lh l n)
+    (if (null? l)
+        n
+        (lh (cdr l) (add1 n))))
+  (lh l 0))
+(my-length '(a b c d))
