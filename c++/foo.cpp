80d45c47c9ee8ddf417fe6edccdd161917515af4
Du Lin <lnidux@gmail.com>
2018-12-02 19:32:56 +0100

    foo.cpp vector

--- c++/foo.cpp
+++ c++/foo.cpp
@@ -4,6 +4,10 @@
 #include<vector>
 #include<numeric>
 
+// FUNCTION PROTOTYPES 
+
+double AddNumbers(double num1, double num2);
+
 int main() {
     std::vector<int> myVec(10);
     std::iota(std::begin(myVec), std::end(myVec), 1);
@@ -13,5 +17,17 @@ int main() {
             std::cout << y << "\n";
     }
 
+    double num1, num2;
+    std::cout << "Enter num 1: ";
+    std::cin >> num1;
+    std::cout << "Enter num 2: ";
+    std::cin >> num2;
+    printf("%.1f + %.1f = %.1f\n", num1, num2, AddNumbers(num1, num2));
+
     return 0;
+}
+
+// FUNCTIONS
+double AddNumbers(double num1, double num2) {
+    return num1 + num2;
 }
