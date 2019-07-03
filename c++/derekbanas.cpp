47f1a432ad4eb14233abb44b96621c64c9694229
Du Lin <lnidux@gmail.com>
2019-07-03 23:31:25 +0200

    Derek Banas tut c++

--- c++/derekbanas.cpp
+++ c++/derekbanas.cpp
@@ -8,6 +8,12 @@ using namespace std;
 int main() {
     
     cout << "Hello World" << endl;
+    
+    const double PI = 3.1415926535;
+    
+    char myGrade = 'A';
+
+    bool isHappy = true;
 
     return 0;
 }
